d524cf9d3e015c50980fb4531463419986bf91bc
Julien Le Bourg <admin@julien.tech>
2019-07-24 14:42:54 +0200

    Update bypass_approvals.groovy

--- bypass_approvals.groovy
+++ bypass_approvals.groovy
@@ -20,56 +20,27 @@ final int auto_approve_field_id = 10601;
 def auto_approve_field = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(auto_approve_field_id);
 
 
-
-
-
-
-
-
 //def customFieldManager = ComponentAccessor.getComponent(CustomFieldManager);
 
-
-
-
+//function to check a Boolean value in Jira
 def select_checkbox_value(int fieldid, value_name ) {
     def optionsManager = ComponentAccessor.getComponent(OptionsManager);
     IssueService issueService = ComponentAccessor.getComponent(IssueService);
     IssueInputParameters issueInputParameters = issueService.newIssueInputParameters()
     def customField=ComponentAccessor.getCustomFieldManager().getCustomFieldObject(fieldid);
-    
 	def user = ComponentAccessor.getJiraAuthenticationContext().loggedInUser
 	Options options = optionsManager.getOptions(customField.getConfigurationSchemes().first().getOneAndOnlyConfig());
-
-	
     def optionToSelect = options.find { it.value == value_name } 
-	
     IssueManager issueManager = ComponentAccessor.getIssueManager();
     issue.setCustomFieldValue(customField, [optionToSelect])
 	issueManager.updateIssue(user, issue, EventDispatchOption.DO_NOT_DISPATCH, false);
-
-
     issue.store();
     }
 
-
-
-def senior_approver_required_field = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(senior_approver_required_field_id);
-//def senior_approver_required_field = customFieldManager.getCustomFieldObjectByName("Senior Management Approval")
-
-
-
-def senior_approver_required = issue.getCustomFieldValue(senior_approver_required_field)
-
-if (senior_approver_required){
-log.info("needs approval")
-}
-
-
-
-
-if (!senior_approver_required) {
-    
-    def JIRA_API_URL = "https://jira"
+def has_recieved_at_least_x_approvals(approvals_needed){
+	// We need to know who has already approved the request. 
+    // we use the api as the java classes dont seem to allow us to know it
+	def JIRA_API_URL = "https://jira"
      
     def jira = new HTTPBuilder(JIRA_API_URL);
     jira.client.addRequestInterceptor(new HttpRequestInterceptor() {
@@ -80,21 +51,44 @@ if (!senior_approver_required) {
     //https://jira/rest/api/latest/issue/BBEITSM-33
     def resp = jira.get(path: '/rest/api/latest/issue/'+issue)
     approvers = resp['fields']['customfield_10000']['approvers']
-    def approved = false
-    for (approver in approvers) {
+ 
+	approvals_count =0
+    for (approver in approvers) { //Loop over approvers, if any has approved, set approved to true
         log.info(""+approver['approver']['displayName'])
         log.info(""+approver['approverDecision'])
-        approved= "approved" in approver['approverDecision']
+		if ("approved" in approver['approverDecision'])
+		{
+			
+			approvals_count +=1
+		}     
         log.info(""+approved)
+    }
+	return approvals_count>=approvals_needed
+}
+
+
+
+
+//SCRIPT START
+
+def senior_approver_required_field = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(senior_approver_required_field_id);
+def senior_approver_required = issue.getCustomFieldValue(senior_approver_required_field)
+// we get the custom field value to determine if it is required to have a senior manager's approval
+
+
+if (senior_approver_required){
+log.info("needs approval")
+
+
+else {
+	
         
-        }
-        
-    def is_already_approved = false
+    //We should auto approve if the request is waiting for an approval (could be already approved)
     def status = resp['fields']['customfield_10001']['currentStatus']['status']
     
     if (status=="Waiting for approval"){
       
-		if (approved) {
+		if (has_recieved_at_least_x_approvals(1)) {
         
      		log.info("auto Approving")
             select_checkbox_value(auto_approve_field_id, "True")
@@ -103,15 +97,5 @@ if (!senior_approver_required) {
        else {
            log.info("already approved")
            }
+	}
 }
-
-
-//def auto_approve_value = issue.getCustomFieldValue(auto_approve_field)
-//issue.setCustomFieldValue(auto_approve_field, true);
-
-
-
-select_checkbox_value(auto_approve_field_id, "True")
-
-
-
